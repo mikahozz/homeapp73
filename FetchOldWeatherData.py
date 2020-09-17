703dbfb7e3d30d46ce3b32d06964bb852c918c97
mikaahopelto <69237295+mikaahopelto@users.noreply.github.com>
2020-09-17 19:45:37 +0300

    Improved documentation

--- FetchOldWeatherData.py
+++ FetchOldWeatherData.py
@@ -1,7 +1,8 @@
 # Databricks notebook source
+# Fetch weather data for the whole year in 7 day batches
 import datetime as dt
 
-startTime = dt.datetime.strptime("2020-01-06 00:00:00", "%Y-%m-%d %H:%M:%S")
+startTime = dt.datetime.strptime("2020-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")
 
 while startTime < dt.datetime.utcnow():
   endTime = startTime + dt.timedelta(minutes=(6*24*60)-10) # Seven days is max
@@ -13,5 +14,6 @@ while startTime < dt.datetime.utcnow():
 
 # COMMAND ----------
 
+# Fetch weather data by exact dates
 params = {"start_time":"2020-09-14T00:00:00.000+0000","end_time":"2020-09-17T16:32:39.520+0000"}
 dbutils.notebook.run("/Users/mika.ahopelto@outlook.com/Homeapp73/weatherdata", 600, params)
