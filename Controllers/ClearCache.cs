0e3f212699c8c1c76447057f8542223e5a2f5383
mikaahopelto <69237295+mikaahopelto@users.noreply.github.com>
2020-09-29 11:58:02 +0300

    Tree image and clear cache

---
+++ Controllers/ClearCache.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace homeapp73.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClearCache : ControllerBase
+    {
+        #region Set up configuration stuff
+        private readonly IConfiguration _configuration;
+        private IMemoryCache _cache;
+        private readonly ILogger _logger;
+        public ClearCache(IConfiguration configuration, IMemoryCache memoryCache, ILogger<ClearCache> logger)
+        {
+            _configuration = configuration;
+            _cache = memoryCache;
+            _logger = logger;
+        }
+
+        #endregion
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string cacheKey = _configuration.GetValue<string>("AppSettings:CacheKey");
+            _cache.Remove(cacheKey);
+            _logger.LogInformation("Cache cleared");
+
+            return StatusCode(200);        
+        }
+    }
+}
